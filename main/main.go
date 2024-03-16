be1dd4ad1f9eb834c72be4265be97874ef9ab510
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2024-03-17 00:54:34 +0200

    adding JWT to the multi-module workspace

--- main/main.go
+++ main/main.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/career-built/marketPlace/Api"
 	"github.com/career-built/marketPlace/Database"
 	"github.com/career-built/marketPlace/MessageBroker"
 	"github.com/career-built/marketPlace/OpenTelemetry"
-	"github.com/career-built/marketPlace/Product_Api"
 	"github.com/career-built/marketPlace/Product_Service"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	fmt.Println("Starting Base")
-	// define the base dbConnector
+	// define the type of dbConnector
 	dbConnector := Database.NewPostgres()
 	if dbConnector == nil {
 		log.Fatal("can't connect to database")
 	}
 	defer dbConnector.CloseDB()
 
-	// define the base Message Broker
+	// define the type of Message Broker
 	messagebroker, err := MessageBroker.NewRabbitMQBroker("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		log.Fatalf("can't connect to messagebroker:: %s", err)
@@ -32,17 +32,19 @@ func main() {
 	//inject the Database to the base Db interface
 	productMgr := Product_Service.NewProductService(dbConnector, messagebroker)
 
-	// define the base Message Broker tracer
+	// define the type of Message Broker tracer
 	messageTracer, err := OpenTelemetry.NewGlobalJaegerTracer("Test-tracing-app")
 	if err != nil {
 		log.Fatalf("can't connect to messageTracer:: %s", err)
 		panic(err)
 	}
 	defer messageTracer.Close()
+	// define the type of JWT
+	jwtobj := JWT.newkeyCloakMiddleware()
 	//inject the product feature to the base manager interface
-	productRouter := Product_Api.NewProductRouter(productMgr, messageTracer)
-
+	productRouter := Api.NewProductRouter(productMgr, messageTracer)
 	e := echo.New()
+
 	e.POST("/product/create", productRouter.CreateProduct)
 	e.GET("/product/:id", productRouter.GetProductByID)
 	e.GET("/getproductfromMarket", productRouter.GetProductFromMarket)
